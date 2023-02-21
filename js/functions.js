8e60819da9a772cbf6125c69cda03a82e66c9320
Slava Kochnev <sl.kochnev@mail.ru>
2023-02-21 21:38:52 +0500

    Нужно больше функций

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,30 @@
 const firstTask = (string, number) => string.length <= number;
+firstTask('проверяемая строка', 20);
 
 const secondTask = (string) => {
-string = string.toLowerCase().replaceAll('', '');
-return string === string.split('').reverse().join('');
+string = string
+  .toLowerCase()
+  .replaceAll('', '');
+return string === string
+  .split('')
+  .reverse()
+  .join('');
 }
+secondTask('топот');
 
 const thirdTask = (string) => {
   if (typeof string === 'number') {
-    return string;
+  return string;
   }
-    let result = '';
+  let result = '';
   for (let i = 0; i < string.length; i++) {
-    if (!Number.isNaN(parseInt(string.at(i), 10))) {
+  if (!Number.isNaN(parseInt(string.at(i), 10))) {
     result += string.at(i);
     }
   }
-    return parseInt(result, 10);
+  return parseInt(result, 10);
 }
+thirdTask('ECMAScript 2022');
 
 const myPadStart = (string, minLength, pad) => {
   const actualPad = minLength - string.length;
