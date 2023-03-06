d530139e4f3116bf3d6f1b1dc2b0cb0c136b3704
Slava Kochnev <sl.kochnev@mail.ru>
2023-03-06 12:23:43 +0500

    модуляция

---
+++ js/data.js
@@ -0,0 +1,51 @@
+import {
+  getRandomInteger,
+  getRandomElement,
+  getSomeRandomElement,
+  createIdRandomGenerator,
+  createIdGenerator
+} from './util-data.js';
+
+const MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  ];
+  const NAMES = ['Саша',
+  'Гриша',
+  'Ваня',
+  'Лёша',
+  'Васёк',
+  'Мишаня',
+  'Герман',
+  'Коля',
+  'Слава',
+  'Лев'];
+  
+const PHOTO_ELEMENT_COUNT = 25;
+const COMMENT_COUNT = 3;
+const idPhoto = createIdGenerator(25);
+const urlPhoto = createIdGenerator(25);
+const idRandomComment = createIdRandomGenerator(999);
+
+const createComment = () => ({
+  id: idRandomComment(),
+  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+  message: getSomeRandomElement(messages, 2),
+  name: getRandomElement(names),
+});
+
+const createPhotoElement = () => ({
+  id: idPhoto(),
+  url: `photos/${urlPhoto()}.jpg`,
+  description: 'Это какое-то описание фотографии',
+  likes: getRandomInteger(15, 200),
+  comments: Array.from({length: COMMENT_COUNT}, createComment),
+});
+
+const createPhotoElements = () => Array.from({length: PHOTO_ELEMENT_COUNT}, createPhotoElement);
+
+export {createPhotoElements};
